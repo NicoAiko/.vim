2f339104b258347dc30e0d0c687f128e9fced4e4
%%GITNAME%% <%%GITEMAIL%%>
2017-07-19 21:09:55 +0200

    Change install bash script

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-timestamp=$(date +%s)
+DATE=`date +%Y-%m-%d`
+cd $( dirname ${BASH_SOURCE[0]} )
+PWD=$(pwd)
 
 if [ -f ~/.vimrc ]; then
-	echo "Renaming current .vimrc to .vimrc-$(timestamp)"
-	mv ~/.vimrc ~/.vimrc-$(timestamp)
+	echo "Renaming current .vimrc to .vimrc-$(DATE)"
+	mv ~/.vimrc ~/.vimrc-$(DATE)
 fi
 
 echo "Creating symlink for .vimrc"
-ln -s ./.vimrc ~/.vimrc
+ln -s $(PWD)/.vimrc ~/.vimrc
 
 if [ -d ~/.vim ]; then
-	echo "Renaming current .vim folder to .vim-$(timestamp)";
-	mv ~/.vim ~/.vim-$(timestamp);
+	echo "Renaming current .vim folder to .vim-$(DATE)";
+	mv ~/.vim ~/.vim-$(DATE);
 fi
 
 echo "Creating symlink for .vim folder"
-ln -s ./.vim ~/.vim
+ln -s $(PWD)/.vim ~/.vim
 
 if [ -f ~/.bash_profile ]; then
-	echo "Renaming current .bash_profile to .bash_profile-$(timestamp)"
-	mv ~/.bash_profile ~/.bash_profile-$(timestamp)
+	echo "Renaming current .bash_profile to .bash_profile-$(DATE)"
+	mv ~/.bash_profile ~/.bash_profile-$(DATE)
 fi
 
 echo "Creating symlink for .bash_profile"
-ln -s ./.bash_profile ~/.bash_profile
+ln -s $(PWD)/.bash_profile ~/.bash_profile
